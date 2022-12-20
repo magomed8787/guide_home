fd1e9e9532a9b4c715604e1407dba58d72417a9e
M <M>
2022-12-20 21:13:21 +0300

    new

--- csv_creat.py
+++ csv_creat.py
@@ -1,2 +1,5 @@
-with open (file, 'w', encoding = 'utf-8') as data:
-    data.write(f'Фамилия;Имя;Номер телефона;Описание\n')
+def creating ():
+    file = 'Phonebook.csv'
+    with open(file, 'w', encoding = 'utf-8') as data:
+        data.write(f'Фамилия;Имя;Номер телефона;Описание\n')
+
---
+++ main.py
@@ -0,0 +1,12 @@
+# from csv_creat import creating
+from file_writing import writing_scv
+from file_writing import writing_txt
+
+
+# path = 'Phonebook.csv'
+# valid = True
+# if not valid:
+#     creating()
+
+writing_scv()
+writing_txt()
